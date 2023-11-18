dc1ff61c7d4ab75201a8a19a1d9069a78670362d
Tedo <tedojanelidze7@gmail.com>
2023-11-18 21:47:49 +0400

    created star rating component api

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import StarRating from './StarRating';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
     <StarRating
       maxRating={5}
       messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
